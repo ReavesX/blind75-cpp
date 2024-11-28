461e55b13fc1d06e8723da5af25ad8325ee7ecd2
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2024-11-28 12:17:22 -0500

    Added the problem description

--- binary/371. Sum of Two Integers.cpp
+++ binary/371. Sum of Two Integers.cpp
@@ -1,3 +1,24 @@
+/*
+
+371. Sum of Two Integers
+
+Problem: Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+Example 1:
+
+    Input: a = 1, b = 2
+    Expected Output: 3
+
+Example 2:
+    Input: a = 2, b = 3
+    Expected Output: 5
+ 
+
+Constraints:
+    -1000 <= a, b <= 1000
+
+*/
+
 class Solution {
 public:
     int carry;
