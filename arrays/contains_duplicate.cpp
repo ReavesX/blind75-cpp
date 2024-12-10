a3b289fbc1b0f0428db4b9f9588061d72317a95e
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2024-12-10 17:30:51 -0500

    Contains Duplicate completed successfully

--- arrays/contains_duplicate.cpp
+++ arrays/contains_duplicate.cpp
@@ -1,1 +1,21 @@
+// Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        // loop through elements in nums
+            // i starts 0
+            // j starts 1
+        // if nums i == nums j -> duplicate 
+            //if duplicate_count > 0: return True
+            //else false 
+        for (int i = 0; i < nums.size(); i++){
+            for (int j = (i+1); j < nums.size(); j++){
+                if (nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
